4bc39f606181bd978467227d8a625cd51607eb99
Caio Cesar Santos <caiocesar2004gfgff@gmail.com>
2023-12-29 23:31:35 -0300

    Avançando...

--- html.php
+++ html.php
@@ -1,3 +1,11 @@
 <!-- Este documento é um estudo de integração entre PHP e HTML. -->
+<?php
 
-<div class="titulo"></div>
+//for loop é tipo: for (condição pra entrar; condição pra parar; oq ele vai fazer enquanto num atingir a condição)
+for ($contagem = 1; $contagem <= 15; $contagem ++){
+    echo "$contagem" . PHP_EOL;
+};
+
+//diferença de while e for: While é para algo mais dinamico como: quer que o usuario pare o loop ou pare o loop quando der erro, sla.
+
+?>
