25cd866dafaf6113b44e008326200512c876d362
Caio Cesar Santos <caiocesar2004gfgff@gmail.com>
2024-01-03 00:01:56 -0300

    Criado novo perfil para estudo de manipulação de array

--- Exercicio-2.php
+++ Exercicio-2.php
@@ -18,7 +18,7 @@ $conta = [
 //ai tu chama ela assim pai
 echo $conta['nome'] . PHP_EOL;
 
-//se tiver varias da pra fazer assim:
+//se tiver varias informações da pra fazer assim:
 echo "=========================================================================". PHP_EOL;
 
 $conta1 = [
@@ -41,4 +41,43 @@ $todasContas = [$conta1, $conta2, $conta3];
 for ($c = 0; $c < count($todasContas); $c++){
     echo $todasContas[$c]['nome'] . PHP_EOL;
 };
+
+echo "========================For Each====================================". PHP_EOL;
+//CONTUDO, tem uma forma mais legivel de escrever isso... Segue o fluxo aq
+foreach ($todasContas as $contas){
+    echo $contas['dinheiro'] . PHP_EOL;
+};
+
+echo "========================Caso voce queira criar o indice:====================================". PHP_EOL;
+$exemplo = [
+    10203040410 => [
+        'nome' => 'julio',
+        'dinheiro' => 2000
+    ],
+    20304050510 => [
+        'nome' => 'maria',
+        'dinheiro' => 3000
+    ]
+];
+
+//ah quero adiiconar novos itens ai nesse array... :( como faço? VEM VER! BAHAHA
+$exemplo[45454545454] = [
+    'nome'=> 'AH sei la paizão - nome novo1',
+    'dinheiro' => 8000 #VEM COM O MONSTRO
+];
+
+foreach ($exemplo as $cpf => $valor) {
+    echo "CPF: " . $cpf . PHP_EOL;
+    echo "Nome: " . $valor['nome'] . PHP_EOL;
+    echo "Dinheiro: " . $valor['dinheiro'] . PHP_EOL;
+    echo "------------------------" . PHP_EOL;
+};#VaiCurintia
+
+//se fosse pra adicionar num array mais simples ou numerico, seria assim:
+$a_simples = [10, 20, 30, 40];
+echo "antes de adcionar:" . print_r($a_simples); #print_r ajuda a imprimir a estrutura do array de uma forma mais adequada - SEM DAR ERRO
+
+$a_simples[] = 60;
+echo "depois:" . print_r($a_simples); // ta bugado e o depois ta saindo literalmente depois do array... enfim, deu pra entender
+
 ?>
---
+++ Exercicio-3.php
@@ -0,0 +1,30 @@
+<?php
+//continuando aq LOL -pq lá ja tava ficando bem bagunçado... ENFIM, VAMOS DAR CONTINUIDADE
+//bora fazer um sistema de saque e deposito em um banquinho de mentirinha rapaz.
+
+$contasCorrente = [
+    '20510221065'=> [
+        "titular"=> "Jonas",
+        "saldo" => 1200    
+    ],
+
+    '10825248755' => [
+        "titular"=> "Helena",
+        "saldo"=> 2000
+    ],
+
+    '215645732566' => [
+        "titular" => "Thiago",
+        "saldo" => 2050
+    ],
+];
+
+// $contasCorrente['123685987440'] = [
+//     "titular" => "Claudia",
+//     "saldo" => 1500
+// ]
+
+foreach ($contasCorrente as $cpf => $conta) {
+    echo "CPF:". $cpf . PHP_EOL . "Titular:". $conta["titular"] . PHP_EOL . "============================" . PHP_EOL;
+}
+?>
