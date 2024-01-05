64f2b55d3c2eb282d1bb83546b7c3c9e207653e5
Caio Cesar Santos <caiocesar2004gfgff@gmail.com>
2024-01-05 10:37:41 -0300

    Adicionando um saquezinho bem pifio e desfuncional, mas é apenas para estudo de funções.

--- Exercicio-3.php
+++ Exercicio-3.php
@@ -2,6 +2,11 @@
 //continuando aq LOL -pq lá ja tava ficando bem bagunçado... ENFIM, VAMOS DAR CONTINUIDADE
 //bora fazer um sistema de saque e deposito em um banquinho de mentirinha rapaz.
 
+//criando pequena função pro cabra n tirar mais dinheiro doq tem e ficar negativo:
+function exibirMensagem($mensagem) {
+    echo $mensagem . PHP_EOL; // - essa vai se a msg"Você não pode sacar por saldo insuficiente." . PHP_EOL;
+}
+
 $contasCorrente = [
     '20510221065'=> [
         "titular"=> "Jonas",
@@ -24,7 +29,20 @@ $contasCorrente = [
 //     "saldo" => 1500
 // ]
 
+//Ah quero sacar o bglh CHEGA MAIS:
+ if(500 > $contasCorrente["20510221065"]["saldo"]){
+    exibirMensagem(mensagem: "Você não pode sacar por saldo insuficiente!");
+ }else{
+    $contasCorrente["20510221065"]["saldo"] -= 500;
+ };
+
+ if(2500 > $contasCorrente["10825248755"]["saldo"]){
+    exibirMensagem(mensagem: "Você não pode sacar por saldo insuficiente!");
+ }else{
+    $contasCorrente["20510221065"]["saldo"] -= 2500;
+ };
+
 foreach ($contasCorrente as $cpf => $conta) {
-    echo "CPF:". $cpf . PHP_EOL . "Titular:". $conta["titular"] . PHP_EOL . "============================" . PHP_EOL;
-}
+    echo "CPF:". $cpf . PHP_EOL . "Titular:". $conta["titular"] . PHP_EOL . "Saldo:" . $conta["saldo"] . PHP_EOL . "============================" . PHP_EOL;
+};
 ?>
