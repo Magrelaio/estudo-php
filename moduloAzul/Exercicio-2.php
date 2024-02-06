fa45d32a5945c2cd6e08d788f7f5d622ade05175
Magrelaio <caiocesar2004gfgff@gmail.com>
2024-02-06 19:14:05 -0300

    Continuando o estudo de manipulação de arrays

--- moduloAzul/Exercicio-2.php
+++ moduloAzul/Exercicio-2.php
@@ -15,12 +15,16 @@ $notas = [
         'nota' => 9,
     ],
 ];
-function ordenaNotas(array $nota1, array $nota2): int
+function ordenaNotas(array $nota1, array $nota2)
 {
-    if($nota1['nota'] > $nota2['nota']){
-        return -1;
-    } return 1;
+    if($nota1['nota'] <=> $nota2['nota']){
+        return 1;
+    }
+};
+//usort(array:$notas, callback:'ordenaNotas'); //usuaruio define com funções como trabalhar com esse array
 
-}
-usort(array:$notas, callback:'ordenaNotas');
+//ai temos como ordenar de diferentes maneiras, como por exemplo:
+//asort(array: $notas); //organizar em ordem alfabética
+//arsort(array: $notas); //o R faz ele fazer os arrays ao contrario (em ordem alfabetica Z-A, em ordem numerica 10-1)
+//rsort(array aqui); mesma coisa que em cima
 var_dump($notas);
